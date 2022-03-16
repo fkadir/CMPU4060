79691ef58681771ef611ae2b7d9093465fa42342
fkadir <99030827+fkadir@users.noreply.github.com>
2022-03-16 15:40:39 +0000

    Add files via upload
    labs in CMPU4060 of TU259 until 16-3-2022

--- Lab Test 2/LabTest2.py
+++ Lab Test 2/LabTest2.py
@@ -7,89 +7,92 @@ def main():
         try:
             # create menu
             i = int(input("What would you like to do?\n"
-                          "1. search for documents\n"
+                          "1. Search for documents\n"
                           "2. Read Document\n"
                           "3. Quit program\n"))
             # call correct function depending on user input
             if i == 1:
                 search_docs(word_dict)
             elif i == 2:
-                i = int(input("Enter document number:"))
-                display_doc(i, doc_dict)
+                doc_input = int(input("Enter document number:"))
+                display_doc(doc_input, doc_dict)
             elif i == 3:
                 exit()
+        except ValueError:
+            print("Invalid input, please try again: ", '\n')
         except KeyError:
-            print('ugh')
-            # i = int(input("Invalid input, please try again: "))           #?
+            print("Invalid input, please try again: ", '\n')
 
 
 # function reads the files and creates word_dict (words and corresponding document numbers)
 # and doc_dict (document numbers and corresponding text)
 def read_files():
-    f = open('ap_docs2.txt', 'r')
+    f = open('ap_docs.txt', 'r')
     text = f.read()
 
-    # list of the documents where the line breaks ar removed
+    # list of the documents where the line breaks are removed
     docs_list = text.split('<NEW DOCUMENT>')
-    docs_list = [s.strip().replace('\n', ' ') for s in docs_list]
+    docs_list = [s.strip() for s in docs_list]      #
     docs_list.remove('')
     f.close()
-
-    # set of all words in all documents
-    word_set = set()
-    for doc in docs_list:
-        # all words in the document are stripped of punctuation and uppercase letters
-        words = doc.strip(string.punctuation).lower().split()
-        for i in range(0, len(words)):
-            word_set.add(words[i])
 
     # doc dict contains document numbers and corresponding text
     doc_dict = dict()
     for j in docs_list:
         doc_dict[docs_list.index(j) + 1] = j
 
+    # set of all words in all documents
+    word_set = set()
+    for doc in docs_list:
+        # all words in the document are stripped of punctuation and uppercase letters
+        words_list = doc.strip(string.punctuation).lower().split()
+        for i in range(0, len(words_list)):
+            word_set.add(words_list[i])
+
     # word dict contains all words and corresponding document numbers the word is present in
-    word_dict = dict()
+    word_dict = {}
     stripped_doc = [x.lower().strip(string.punctuation) for x in doc_dict.values()]
     for k in stripped_doc:
-        for l in word_set:
-            if l in k:
-                if l in word_dict:
-                    word_dict[l] = word_dict[l] + ', ' + str(stripped_doc.index(k) + 1)
+        for ws in word_set:
+            if ws in k:
+                # to make sure we add the word only once to the dictionary
+                # and have all the document numbers corresponding to one key
+                if ws in word_dict:
+                    word_dict[ws].add(stripped_doc.index(k) + 1)
                 else:
-                    word_dict[l] = set(stripped_doc.index(k) + 1)       #?
-                    print(word_dict[l])
-                # if l in word_dict:
-                #     word_dict[l] = word_dict[l] + ', ' + str(stripped_doc.index(k) + 1)
-                # else:
-                #     word_dict[l] = str(stripped_doc.index(k) + 1)
-    print(word_dict)
+                    word_dict[ws] = {stripped_doc.index(k) + 1}
     return word_dict, doc_dict
 
 
+# function that takes user search input and returns relevant document numbers
 def search_docs(word_dict):
-    k = input("Enter search words: ")
-    k = k.lower().strip()
-    search_words = k.split()
-    doc_sets_list = []
-    m = [word_dict[w] for w in search_words if w in word_dict]
-    for sw in m:
-        sw = sw.split(',')
-        sw = {x.strip() for x in sw}
-        doc_sets_list.append(sw)
-    result = list(set.intersection(*doc_sets_list))
-    result.sort()
-    result = ' '.join(result)
-    print('Documents fitting search:')
-    print(result, '\n')
+    try:
+        k = input("Enter search words: ")
+        k = k.lower().strip(string.punctuation)
+        search_words = k.split()
+        doc_sets_list = [word_dict[w] for w in search_words if w in word_dict]
+        result = list(set.intersection(*doc_sets_list))
+
+        # when the result list is empty rais an exception
+        if len(result) == 0:
+            raise ValueError
+
+        # formatting results to print
+        result.sort()
+        res = [str(a) for a in result]
+        res = ' '.join(res)
+
+        print('Documents fitting search:', res, sep='\n')
+    except TypeError:
+        print("No relevant documents found", '\n')
+    except ValueError:
+        print("No relevant documents found", '\n')
 
 
+# function to display a specific document
 def display_doc(doc_number, doc_dict):
-    sentences = doc_dict[doc_number].replace('.', '.\n')
-    print("Document #{}".format(doc_number))
-    print("-" * 25)
-    print(sentences)
-    print("-" * 25)
+    f = "-" * 25
+    print("Document #{}".format(doc_number), f, doc_dict[doc_number], f, sep='\n')
 
 
 main()
